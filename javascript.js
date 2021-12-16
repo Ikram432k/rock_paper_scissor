0a8a0d435232d21740bb11f0e663e50449145a45
ikram mohideen <ikram432k@gmail.com>
2021-12-16 17:40:21 +0530

    ADD javascript.js

--- javascript.js
+++ javascript.js
@@ -1,44 +1,90 @@
+let humanCount = 0;
+let systemCount = 0;
+
+
+    const value = document.querySelectorAll(".things");
+    const finalResult = document.querySelector('#finalResult');
+    const computerScore = document.querySelector('#computerScore');
+    const playerScore = document.querySelector('#playerScore');
+
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];}
 
-
-function playround(playerSelection,computerSelection){
+function playround(playerSelection){
+    const computerSelection = computerPlay(); 
     
     if(playerSelection=='rock'&&computerSelection=='paper'){
-    console.log("computer won the game");
+        systemCount++;
+        computerScore.textContent=systemCount;
+        finalResult.textContent="computer won the game";
+
     } 
     else if (playerSelection=='rock'&&computerSelection=='scissors'){
-    console.log("you won the game");
+        humanCount++;
+        playerScore.textContent=humanCount;
+        finalResult.textContent="you won the game";
+
     }
     else if (playerSelection=='rock'&&computerSelection=='rock'){
-        console.log("game is draw");
+        finalResult.textContent="game is draw";
     }
     else if (playerSelection=='paper'&&computerSelection=='paper'){
-        console.log("game is draw");
+        finalResult.textContent="game is draw";
     }
     else if (playerSelection=='paper'&&computerSelection=='scissors'){
-        console.log("computer won the game");
+        systemCount++;
+        computerScore.textContent=systemCount;
+        finalResult.textContent="computer won the game";
     }
     else if (playerSelection=='paper'&&computerSelection=='rock'){
-        console.log("you won the game");
+        humanCount++;
+        playerScore.textContent=humanCount;
+        finalResult.textContent="you won the game";
+
     }
-    else if (playerSelection=='scissors'&&computerSelection=='scissors'){
-        console.log("game is draw");
+    else if (playerSelection=='scissor'&&computerSelection=='scissor'){
+        finalResult.textContent="game is draw";
     }
-    else if (playerSelection=='scissors'&&computerSelection=='paper'){
-        console.log("you won the game");
+    else if (playerSelection=='scissor'&&computerSelection=='paper'){
+        humanCount++;
+        playerScore.textContent=humanCount;
+        finalResult.textContent="you won the game";
+
     }
-    else if (playerSelection=='scissors'&&computerSelection=='rock'){
-        console.log("computer won the game");
+    else if (playerSelection=='scissor'&&computerSelection=='rock'){
+        systemCount++;
+        computerScore.textContent=systemCount;
+        finalResult.textContent="computer won the game";
+
     }
 }
 
-const computerSelection = computerPlay();
-console.log(playround(playerSelection,computerSelection));
-let n= 5;
+/*
+//console.log(playround(playerSelection,computerSelection));
+/*let n= 5;
 for (i=0;i<5;i++){
-    var playerSelection = window.prompt('whats your choice').toLowerCase();
-    console.log(playround(playerSelection,computerSelection));
+}
+var playerSelection = window.prompt('whats your choice rock,paper or scissors').toLowerCase();
+console.log(playround(playerSelection,computerSelection));
+*/
+value.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        if(humanCount>=5||systemCount>=5){
+            final();
+        }
+        else{
+  
+        playround(button.dataset.rps);
+        }
+    });
+    });
+function final(){
+    if(humanCount>systemCount)
+    finalResult.textContent="you win";
+    else if(humanCount<systemCount)
+    finalResult.textContent="you lose";
+    else
+    finalResult.textContent="match draw";
 
 }
